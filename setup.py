abae37fe1639e2bc8faed8e3d82745902a9bd77f
Robin Hayman <robin.hayman@gmail.com>
2022-02-18 15:54:16 +0000

    typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="r2p2py",
-    version=0.21,
+    version=0.22,
     author="robin hayman",
     author_email="robin.hayman@gmail.com",
     description="Analysis of 2P data",
